ba8be13b5e7221f62483352da9572e1855c552c5
noakwollsater <142594779+noakwollsater@users.noreply.github.com>
2024-02-13 12:13:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let inputList = []
 let label = document.querySelector("label")
 
 form.addEventListener("submit", AddTodoElement )
+bigGrid.addEventListener("click", removeTodoElement)
+bigGrid.addEventListener("click", ifChecked)
 
 function showOrHideLabel() {
     label.style.display = inputList.length > 0 ? "inline-block" : "none"
@@ -32,14 +34,37 @@ function createTodoElement() {
         let todoDiv = document.createElement("div")
         let listItem = document.createElement("p")
         let checkBox  = document.createElement("input")
+        let removeButton = document.createElement("button")
         checkBox.type = "checkbox";
         listItem.innerText = inputList[i]
+        removeButton.innerText = "X"
 
         todoDiv.appendChild(checkBox)
         todoDiv.appendChild(listItem)
+        todoDiv.appendChild(removeButton)
         bigGrid.appendChild(todoDiv)
+    }   
+}
+
+function removeTodoElement() {
+    if (event.target.tagName === "BUTTON")
+    {
+        let index = inputList.indexOf(event.target.previousElementSibling.innerText)
+        inputList.splice(index, 1)
+        createTodoElement()
     }
-    
 }
 
 
+function ifChecked(){
+    let checked = document.querySelectorAll("input[type=checkbox]:checked")
+    for (let i = 0; i < checked.length; i++)
+    {
+        checked[i].nextElementSibling.style.textDecoration = "line-through"
+    }
+    let unchecked = document.querySelectorAll("input[type=checkbox]:not(:checked)")
+    for (let i = 0; i < unchecked.length; i++)
+    {
+        unchecked[i].nextElementSibling.style.textDecoration = "none"
+    }
+}
